5e11a007c4274e4adc5adcbdbb366e04b60eff68
WeiChen Lin <asdfg09487@gmail.com>
2024-03-16 00:01:03 +0800

    :sparkles: Add login components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { ModeToggle } from '@/components/ui/toggle'
 import { GithubLogo, FacebookLogo, GoogleLogo } from '@phosphor-icons/react'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 export default function Index() {
   const handleSignIn = async () => {
@@ -16,9 +17,9 @@ export default function Index() {
 
   return (
     <div className='flex h-screen items-center justify-center'>
-      <div className='w-full max-w-[600px]'>
-        <header className='w-full py-4 lg:py-6 xl:py-8'>
-          <div className='container flex items-center justify-around gap-x-6 px-4 md:px-6'>
+      <div className='w-full max-w-[600px] flex flex-col justify-between h-full'>
+        <header className='w-full py-4 lg:py-6 xl:py-8 mt-12'>
+          <div className='container flex items-center justify-around gap-x-6'>
             <div className='flex items-center gap-x-6'>
               <Link href='/'>
                 <Image src='/icon.png' width={40} height={40} alt=''></Image>
@@ -28,12 +29,12 @@ export default function Index() {
             <ModeToggle />
           </div>
         </header>
-        <section className='w-full py-4'>
-          <div className='container flex items-center justify-center gap-4 px-4 md:px-6'>
-            <Image src='/stock-bg.svg' alt='home page' width={350} height={350}></Image>
-          </div>
+        <section className='py-4 container flex flex-col items-center justify-center gap-8 px-4 md:px-6 w-[400px]'>
+          <Input placeholder='Email' type='email' />
+          <Input placeholder='Password' type='password' />
+          <Button className='w-full'>Sign In</Button>
         </section>
-        <footer className='w-full'>
+        <footer className='w-full mb-4'>
           <div className='container flex flex-col items-center justify-center py-8 text-center md:flex-row md:space-x-2 md:space-y-0 md:py-12 lg:py-16 xl:py-24'>
             <p className='text-sm text-gray-500 dark:text-gray-400'>© 2024 WeiChen Lin. All rights reserved.</p>
           </div>
