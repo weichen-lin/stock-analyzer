35fdfe26e90ee6d0b420b04e53c3a36a8507572f
WeiChen Lin <asdfg09487@gmail.com>
2024-05-31 17:34:45 +0800

    :sparkles: Add url env

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -10,11 +10,25 @@ import { GoogleLogo } from '@phosphor-icons/react'
 export default function Index() {
   const supabase = createClientComponentClient()
 
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      'http://localhost:3000/'
+    // Make sure to include `https://` when not localhost.
+    url = url.includes('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+    return url
+  }
+
   const handleSignUp = async () => {
+    const url = getURL()
+
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}auth/callback`,
+        redirectTo: `${url}/auth/callback`,
         queryParams: { next: '/lists' },
       },
     })
