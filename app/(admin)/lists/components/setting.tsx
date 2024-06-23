53cc8ad3aa25ae8ba423330619bce6ceae73a231
WeiChen Lin <asdfg09487@gmail.com>
2024-06-23 16:55:21 +0800

    :art: Finish desktop mode

--- app/(admin)/lists/components/setting.tsx
+++ app/(admin)/lists/components/setting.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { DotsThreeVertical } from '@phosphor-icons/react'
+import { DotsThreeVertical, PaperPlaneRight } from '@phosphor-icons/react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import currency from 'currency.js'
 import Link from 'next/link'
 import clsx from 'clsx'
 import { ISetting } from './type'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 const regionMapper: { [key: string]: string } = {
   tw: '台股',
@@ -15,13 +16,13 @@ const regionMapper: { [key: string]: string } = {
 
 export default function Setting(props: ISetting) {
   const { id, name, total, region, update_at } = props
+  const [_name, setName] = useState(name)
 
   const updatedAt = new Date(update_at)
 
   return (
-    <Link href={`/setting/${props.id}`} className='w-full md:w-[330px]'>
-      <motion.div
-        whileHover={{ scale: 1.02 }}
+    <div className='w-full md:w-[330px]'>
+      <div
         className={clsx(
           'flex flex-col items-center gap-x-2 justify-between relative shadow-sm w-full',
           'hover:bg-slate-100 rounded-md border p-2 gap-y-4 dark:border-slate-100',
@@ -29,9 +30,14 @@ export default function Setting(props: ISetting) {
         )}
       >
         <div className='pr-2 pl-3 w-full truncate text-lg flex justify-between items-center'>
-          <div className='truncate max-w-[270px]'>{name}</div>
-          <div className='text-sm text-slate-500 border-slate-300 border-[1px] px-2 py-1 rounded-md'>
-            {regionMapper[region]}
+          <div className='truncate max-w-[270px]'>{_name}</div>
+          <div className='flex gap-x-4 items-center'>
+            <Link href={`/setting/${id}`}>
+              <PaperPlaneRight className='h-6 w-6 text-slate-500' />
+            </Link>
+            <div className='text-sm text-slate-500 border-slate-300 border-[1px] px-2 py-1 rounded-md'>
+              {regionMapper[region]}
+            </div>
           </div>
         </div>
         <div className='flex gap-x-4 justify-between w-full'>
@@ -45,16 +51,14 @@ export default function Setting(props: ISetting) {
                 <DotsThreeVertical className='h-6 w-6' />
               </DropdownMenuTrigger>
               <DropdownMenuContent>
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                <DropdownMenuItem>重新命名</DropdownMenuItem>
+                <DropdownMenuItem>刪除</DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
         </div>
-      </motion.div>
-    </Link>
+      </div>
+    </div>
   )
 }
 
